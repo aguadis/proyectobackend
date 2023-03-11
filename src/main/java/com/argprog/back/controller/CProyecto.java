3c64825455d82a9787a9b8e6837220d4fba51812
aguadis <guadi1404@gmail.com>
2023-03-10 21:22:06 -0300

    cambios

--- src/main/java/com/argprog/back/controller/CProyecto.java
+++ src/main/java/com/argprog/back/controller/CProyecto.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
  @RestController
-@CrossOrigin(origins = {"http://portfoliocintianecol.web.app/"})// la ruta se cruza con angularpackage com.argprog.back.controller;
+@CrossOrigin(origins = {"http://portfoliocintianecol.web.app"})// la ruta se cruza con angularpackage com.argprog.back.controller;
 
 public class CProyecto {
    @Autowired
