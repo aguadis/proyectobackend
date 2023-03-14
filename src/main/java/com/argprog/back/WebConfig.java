46842dafcbc6a51ec414a50bae008e4919670d2f
aguadis <guadi1404@gmail.com>
2023-03-14 04:00:11 -0300

    cors

--- src/main/java/com/argprog/back/WebConfig.java
+++ src/main/java/com/argprog/back/WebConfig.java
@@ -12,12 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        registry.addMapping("/**");
                 
-                .allowCredentials(true)
-                .maxAge(3600);
     }
 
     
