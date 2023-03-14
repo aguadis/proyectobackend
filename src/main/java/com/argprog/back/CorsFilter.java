7c3e239b4e81ecdfd3dce3c1536a1b68df6f3981
aguadis <guadi1404@gmail.com>
2023-03-14 03:22:12 -0300

    cors

--- src/main/java/com/argprog/back/CorsFilter.java
+++ src/main/java/com/argprog/back/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://portfoliocintianecol.web.app"); 
+        response.setHeader("Access-Control-Allow-Origin", "https://portfolio-cintia-necol.onrender.com"); 
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
